fc76fd7f9bdc1223fa0d687ebf7764ed53d2316c
Tuan Nguyen <tuan@pezzie.com>
2025-02-03 23:32:37 +0200

    Remove retry 2 times

--- IntegrationBusiness/ConfigExtentions/HttpConfigurations.cs
+++ IntegrationBusiness/ConfigExtentions/HttpConfigurations.cs
@@ -8,25 +8,6 @@ public static class HttpConfigurations
 {
     public static IServiceCollection AddHttpResiliencePipeline(this IServiceCollection services)
     {
-        services.AddResiliencePipeline("retry2Times", builder =>
-        {
-            builder.AddRetry(new RetryStrategyOptions
-            {
-                Delay = TimeSpan.FromSeconds(1),
-                MaxRetryAttempts = 2,
-                ShouldHandle = new PredicateBuilder()
-                    .Handle<NotFoundException>()
-                    .Handle<FileNotFoundException>(),
-                BackoffType = DelayBackoffType.Exponential,
-                UseJitter = true,
-                OnRetry = retryArguments =>
-                {
-                    Console.WriteLine($"Screenshot failed with exception {retryArguments.Outcome.Exception.Message}. " +
-                                      $"Waiting {retryArguments.Duration} before next retry. Retry attempt {retryArguments.AttemptNumber}");
-                    return ValueTask.CompletedTask;
-                }
-            }).AddTimeout(TimeSpan.FromMinutes(0.2));
-        });
         services.AddResiliencePipeline("retrie5Times", builder =>
         {
             builder.AddRetry(new RetryStrategyOptions
